e665e4f92c6f57367248dfbd83d3fcacf26726c7
295966071 <295966071@qq.com>
2017-07-06 17:17:11 +0800

    update

--- AliPayTest/test/CardTest.aspx.cs
+++ AliPayTest/test/CardTest.aspx.cs
@@ -16,16 +16,17 @@ namespace AliPayTest.test
         IAopClient client = new DefaultAopClient(Config.serverUrl, Config.appId, Config.merchant_private_key, "json", "1.0", "RSA2", Config.alipay_public_key, "utf-8", false);
         protected void Page_Load(object sender, EventArgs e)
         {
+            //创建卡券模板
             AlipayPassTemplateAddRequest request = new AlipayPassTemplateAddRequest();
             AlipayPassTemplateAddModel model = new AlipayPassTemplateAddModel();
-
-            request.BizContent = "{" +
-            "\"unique_id\":\"20140709150010\"," +
-            "\"tpl_content\":\"{\\\"logo\\\": \\\"\\\",\\\"strip\\\": null,\\\"icon\\\": null,\\\"content\\\": {}}\"" +
-            "  }";
+            model.UniqueId = "234774848495696";
+            string File = Server.MapPath("~/test/json.txt");
+            string CardString = System.IO.File.ReadAllText(File);
+            model.TplContent= CardString;
+           
+            request.SetBizModel(model);
             AlipayPassTemplateAddResponse response = client.Execute(request);
-            Console.WriteLine(response.Body);
-
+            Response.Write(response.Body);
         }
     }
 }
