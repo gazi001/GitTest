2735b94f576f5f86cbf8f78e08b9d3275f4de70c
295966071 <295966071@qq.com>
2017-07-06 17:18:50 +0800

    tianjia

---
+++ AliPayTest/test/NewCard.aspx.cs
@@ -0,0 +1,34 @@
+﻿using AliPayTest.Com;
+using Aop.Api;
+using Aop.Api.Request;
+using Aop.Api.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Aop.Api.Domain;
+namespace AliPayTest.test
+{
+    public partial class NewCard : System.Web.UI.Page
+    {
+        IAopClient client = new DefaultAopClient(Config.serverUrl, Config.appId, Config.merchant_private_key, "json", "1.0", "RSA2", Config.alipay_public_key, "utf-8", false);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //新建卡
+            AlipayPassInstanceAddRequest request = new AlipayPassInstanceAddRequest();
+            AlipayPassInstanceAddModel model = new AlipayPassInstanceAddModel();
+           
+
+
+            request.BizContent = "{" +
+            "\"tpl_id\":\"2017070616555935516915129\"," +
+            "\"tpl_params\":\"{\\\"message\\\":\\\"https://alipass.alipay.com//temps/free/logo.png\\\"}\"," +
+            "\"recognition_type\":\"1\"," +
+            "\"recognition_info\":\"{\\\"partner_id\\\":\\\"2016072900114972\\\",\\\"out_trade_no\\\":\\\"1234567\\\"}\"" +
+            "  }";
+            AlipayPassInstanceAddResponse response = client.Execute(request);
+        }
+    }
+}
