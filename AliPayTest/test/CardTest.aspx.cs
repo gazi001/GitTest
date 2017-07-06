9b8657e87a8562dd6e13865d885d61427f17f923
295966071 <295966071@qq.com>
2017-07-06 15:59:39 +0800

    add

--- AliPayTest/test/CardTest.aspx.cs
+++ AliPayTest/test/CardTest.aspx.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Aop.Api;
+using Aop.Api.Request;
+using Aop.Api.Response;
+using AliPayTest.Com;
+using Aop.Api.Domain;
 namespace AliPayTest.test
 {
     public partial class CardTest : System.Web.UI.Page
     {
+        IAopClient client = new DefaultAopClient(Config.serverUrl, Config.appId, Config.merchant_private_key, "json", "1.0", "RSA2", Config.alipay_public_key, "utf-8", false);
         protected void Page_Load(object sender, EventArgs e)
         {
+            AlipayPassTemplateAddRequest request = new AlipayPassTemplateAddRequest();
+            AlipayPassTemplateAddModel model = new AlipayPassTemplateAddModel();
+
+            request.BizContent = "{" +
+            "\"unique_id\":\"20140709150010\"," +
+            "\"tpl_content\":\"{\\\"logo\\\": \\\"\\\",\\\"strip\\\": null,\\\"icon\\\": null,\\\"content\\\": {}}\"" +
+            "  }";
+            AlipayPassTemplateAddResponse response = client.Execute(request);
+            Console.WriteLine(response.Body);
 
         }
     }
